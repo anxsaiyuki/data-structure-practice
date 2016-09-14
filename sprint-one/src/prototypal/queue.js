6de93597f97d9f522d8b4ad3e3ee72aa30428088
Hack Reactor Students <accounts@hackreactor.com>
2016-09-14 16:29:12 -0700

    finished through psuedo stack

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queueObj = Object.create(queueMethods);
+  queueObj.next = 0;
+  queueObj.oldest = 0;
+  queueObj.storage = {};
+  return queueObj;
 };
 
 var queueMethods = {};
 
+queueMethods.size = function() {
+  return this.next - this.oldest;
+}
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.next] = value;
+  this.next++;
+}
+
+queueMethods.dequeue = function() {
+  if (this.size() === 0) return;
+  var deleted = this.storage[this.oldest];
+  delete this.storage[this.oldest];
+  this.oldest++;
+  return deleted;
+}
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.numOfKeys = 0;
+  this.storage = {};
 };
 
+Stack.prototype.size = function() {
+  return this.numOfKeys;
+}
 
+
+
+Stack.prototype.push = function(value) {
+  this.storage[++this.numOfKeys] = value;
+}
+
+
+Stack.prototype.pop = function() {
+  if(this.size() == 0) return;
+  var deleted = this.storage[this.numOfKeys];
+  delete this.storage[this.numOfKeys];
+  this.numOfKeys --;
+  return deleted;
+}
