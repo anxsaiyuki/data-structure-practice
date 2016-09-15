c06289bd1df70edae4deaa6004a8e75bd6d30bb8
Hack Reactor Students <accounts@hackreactor.com>
2016-09-15 10:05:12 -0700

    finished through Hashtables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,48 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (var i = 0; i < this._limit; i++) {
+    var bucket = k + i % this._limit;
+    var tupleItem = this._storage[bucket];
+    if (tupleItem === undefined) {
+      this._storage[bucket] = [k, v];
+      return;
+    }
+    if (tupleItem[0] === k) {
+      this._storage[bucket] = [k, v];
+      return;
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (var i = 0; i < this._limit; i++) {
+    var bucket = k + i % this._limit;
+    var tupleItem = this._storage[bucket];
+    if (tupleItem === undefined) {
+      return undefined;
+    }
+    if (tupleItem[0] === k) {
+      return tupleItem[1];
+    }
+  }
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (var i = 0; i < this._limit; i++) {
+    var bucket = k + i % this._limit;
+    var tupleItem = this._storage[bucket];
+    if (tupleItem === undefined) {
+      return;
+    }
+    if (tupleItem[0] === k) {
+      delete this._storage[bucket];
+      return;
+    }
+  }
 };
 
 
