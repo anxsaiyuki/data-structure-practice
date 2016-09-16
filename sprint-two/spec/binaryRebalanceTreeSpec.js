c31b5c06efdae6b8e7ebfcb8948487d9fb0e7c33
Hack Reactor Students <accounts@hackreactor.com>
2016-09-15 17:41:43 -0700

    check in for dinner

--- sprint-two/spec/binaryRebalanceTreeSpec.js
+++ sprint-two/spec/binaryRebalanceTreeSpec.js
@@ -1,9 +1,24 @@
 describe('binaryReblanceTree', function() {
   var rebalanceTree = BinaryRebalanceTree(2);
 
-  it('rebalance tree depth', function() {
+  it('should insert values at the correct location in the tree', function() {
     rebalanceTree.insert(1);
     rebalanceTree.insert(4);
-    expect(rebalanceTree.getMaxDepth()).to.equal(2);
+    rebalanceTree.insert(3);
+    rebalanceTree.insert(5);
+    expect(rebalanceTree.left.value).to.equal(1);
+    expect(rebalanceTree.right.left.value).to.equal(3);
+    expect(rebalanceTree.right.right.value).to.equal(5);
+
+  });
+
+  it('should determine the tree depth correctly', function() {
+    expect(rebalanceTree.getMaxDepth()).to.equal(3);
+    expect(rebalanceTree.getMinDepth()).to.equal(2);
+  });
+
+  it('should rebalance the tree', function() {
+    rebalanceTree.insert(6);
+    expect(rebalanceTree.getMaxDepth()).to.equal(3);
   });
 });
--- sprint-two/src/binaryRebalanceTree.js
+++ sprint-two/src/binaryRebalanceTree.js
@@ -4,13 +4,48 @@ var BinaryRebalanceTree = function(value) {
   binTree.right = null;
   binTree.parent = null;
   binTree.value = value;
+  binTree.minDepth = 1;
+  binTree.maxDepth = 1;
   return binTree;
 };
 
 var rebalanceTreeMethods = {};
 
+
 rebalanceTreeMethods.insert = function(node) {
+  this._insert(node);
+
+  if ( (this.getMaxSide('right') - this.getMinSide('left')) === 2) {
+    this.rebalanceLeft();
+  } else if ( (this.getMaxSide('left') - this.getMinSide('right')) === 2) {
+    this.rebalanceRight();
+  }
+};
+
+rebalanceTreeMethods.rebalanceRight = function(node) {
+};
+
+rebalanceTreeMethods.rebalanceLeft = function(node) {
+  if(right.left != null ) {
+    this.setAsReplacement(this.right.left);
+  } else {
+
+  }
+};
+
+rebalanceTreeMethods.setAsReplacement = function(tree) {
+  if(this.parent != null) {
+    if (this.parent.left === this) {
+      this.parent.left = tree;
+    }
+  }
+};
+
+
+rebalanceTreeMethods._insert = function(node) {
   var newTree = BinaryRebalanceTree(node);
+  newTree.parent = this;
+
   if(node > this.value) {
     if(this.right === null) {
       this.right = newTree;
@@ -24,8 +59,27 @@ rebalanceTreeMethods.insert = function(node) {
       this.left.insert(node);
     }
   }
-
+  this.updateDepth();
 };
+
+rebalanceTreeMethods.updateDepth = function() {
+  this.minDepth = 1 + Math.min(this.getMinSide('left'), this.getMinSide('right'));
+  this.maxDepth = 1 + Math.max(this.getMaxSide('left'), this.getMaxSide('right'));
+};
+
+
+rebalanceTreeMethods.getMinSide = function(prop) {
+  if(this[prop]) {return this[prop].minDepth;}
+  return 0;
+};
+
+rebalanceTreeMethods.getMaxSide = function(prop) {
+  if(this[prop]) {return this[prop].maxDepth;}
+  return 0;
+};
+
+
+
 
 rebalanceTreeMethods.contains = function(node) {
   if(node === this.value) {
@@ -54,25 +108,12 @@ rebalanceTreeMethods.depthFirstLog = function(cb) {
   if(this.right != null) {
     this.right.depthFirstLog(cb);
   }
-
-  rebalanceTreeMethods.getMaxDepth = function(node) {
-    var currentLevel = 1;
-    var nextLevelNodes = [];
-    var currentLevelNodes = [this];
-
-    while(true) {
-      while(currentLevelNodes.length > 0) {
-        var node = currentLevelNodes.pop();
-        if (node.left) {nextLevelNodes.push(node.left);}
-        if (node.right) {nextLevelNodes.push(node.right);}
-      }
-      if (nextLevelNodes.length === 0) { return currentLevel; }
-      currentLevelNodes = nextLevelNodes;
-      currentLevel ++;
-    }
-  }
 };
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+rebalanceTreeMethods.getMaxDepth = function(node) {
+  return this.maxDepth;
+};
+
+rebalanceTreeMethods.getMinDepth = function(node) {
+  return this.minDepth;
+};
