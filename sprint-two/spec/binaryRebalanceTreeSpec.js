b2f25388aa744cbd9dffb785f80e09fd1e0dee5f
Hack Reactor Students <accounts@hackreactor.com>
2016-09-17 11:38:53 -0700

    finished binary tree with rebalancing

--- sprint-two/spec/binaryRebalanceTreeSpec.js
+++ sprint-two/spec/binaryRebalanceTreeSpec.js
@@ -3,59 +3,58 @@ describe('binaryRebalanceTree', function() {
   var rebalanceTree = BinaryRebalanceTree(2);
 
   
-  it('should insert values at the correct location in the tree', function() {
+  it('should insert keys at the correct location in the tree', function() {
     rebalanceTree = rebalanceTree.insert(1);
     rebalanceTree = rebalanceTree.insert(4);
     rebalanceTree = rebalanceTree.insert(3);
     rebalanceTree = rebalanceTree.insert(5);
-    expect(rebalanceTree.left.value).to.equal(1);
-    expect(rebalanceTree.right.left.value).to.equal(3);
-    expect(rebalanceTree.right.right.value).to.equal(5);
+    expect(rebalanceTree.left.key).to.equal(1);
+    expect(rebalanceTree.right.left.key).to.equal(3);
+    expect(rebalanceTree.right.right.key).to.equal(5);
 
   });
 
   it('should determine the tree depth correctly', function() {
     expect(rebalanceTree.getMaxDepth()).to.equal(3);
-    expect(rebalanceTree.getMinDepth()).to.equal(2);
   });
 
   it('should rebalance the tree left with beta at root', function() {
     rebalanceTree = rebalanceTree.insert(6);
     expect(rebalanceTree.getMaxDepth()).to.equal(3);
-    expect(rebalanceTree.value).to.equal(4);
-    expect(rebalanceTree.left.value).to.equal(2);
-    expect(rebalanceTree.left.left.value).to.equal(1);
-    expect(rebalanceTree.left.right.value).to.equal(3);
-    expect(rebalanceTree.right.value).to.equal(5);
+    expect(rebalanceTree.key).to.equal(4);
+    expect(rebalanceTree.left.key).to.equal(2);
+    expect(rebalanceTree.left.left.key).to.equal(1);
+    expect(rebalanceTree.left.right.key).to.equal(3);
+    expect(rebalanceTree.right.key).to.equal(5);
     expect(rebalanceTree.right.left).to.equal(null);
-    expect(rebalanceTree.right.right.value).to.equal(6);
+    expect(rebalanceTree.right.right.key).to.equal(6);
   });
 
 
   it('should rebalance with special case of no right node', function() {
-    rebalanceTree = rebalanceTree.insert(5);
+    rebalanceTree = BinaryRebalanceTree(5);
     rebalanceTree = rebalanceTree.insert(1);
     rebalanceTree = rebalanceTree.insert(3);
 
-    expect(rebalanceTree.value).to.equal(3);
-    expect(rebalanceTree.left.value).to.equal(1);
-    expect(rebalanceTree.right.value).to.equal(5);
+    expect(rebalanceTree.key).to.equal(3);
+    expect(rebalanceTree.left.key).to.equal(1);
+    expect(rebalanceTree.right.key).to.equal(5);
   });
 
 
 
   it('should rebalance the tree left without beta at root', function() {
     rebalanceTree = BinaryRebalanceTree(1);
-    expect(rebalanceTree.value).to.equal(1);
+    expect(rebalanceTree.key).to.equal(1);
     rebalanceTree = rebalanceTree.insert(2);
-    expect(rebalanceTree.right.value).to.equal(2);
+    expect(rebalanceTree.right.key).to.equal(2);
 
     rebalanceTree = rebalanceTree.insert(3);
-    expect(rebalanceTree.value).to.equal(2);
-    expect(rebalanceTree.left.value).to.equal(1);
+    expect(rebalanceTree.key).to.equal(2);
+    expect(rebalanceTree.left.key).to.equal(1);
     expect(rebalanceTree.left.right).to.equal(null);
     expect(rebalanceTree.left.left).to.equal(null);
-    expect(rebalanceTree.right.value).to.equal(3);
+    expect(rebalanceTree.right.key).to.equal(3);
     expect(rebalanceTree.right.right).to.equal(null);
 
     expect(rebalanceTree.getMaxDepth()).to.equal(2);
@@ -68,27 +67,27 @@ describe('binaryRebalanceTree', function() {
     rebalanceTree = rebalanceTree.insert(4);
     rebalanceTree = rebalanceTree.insert(5);
 
-    expect(rebalanceTree.value).to.equal(2);
-    expect(rebalanceTree.left.value).to.equal(1);
+    expect(rebalanceTree.key).to.equal(2);
+    expect(rebalanceTree.left.key).to.equal(1);
     expect(rebalanceTree.left.left).to.equal(null);
     expect(rebalanceTree.left.right).to.equal(null);
-    expect(rebalanceTree.right.value).to.equal(4);
+    expect(rebalanceTree.right.key).to.equal(4);
     expect(rebalanceTree.right.left).to.equal(null);
-    expect(rebalanceTree.right.right.value).to.equal(5);
+    expect(rebalanceTree.right.right.key).to.equal(5);
     expect(rebalanceTree.right.right.left).to.equal(null);
     expect(rebalanceTree.right.right.right).to.equal(null);
 
     //debugger;
     rebalanceTree = rebalanceTree.insert(6);
-    expect(rebalanceTree.value).to.equal(2);
-    expect(rebalanceTree.left.value).to.equal(1);
+    expect(rebalanceTree.key).to.equal(2);
+    expect(rebalanceTree.left.key).to.equal(1);
     expect(rebalanceTree.left.left).to.equal(null);
     expect(rebalanceTree.left.right).to.equal(null);
-    expect(rebalanceTree.right.value).to.equal(5);
-    expect(rebalanceTree.right.left.value).to.equal(4);
+    expect(rebalanceTree.right.key).to.equal(5);
+    expect(rebalanceTree.right.left.key).to.equal(4);
     expect(rebalanceTree.right.left.left).to.equal(null);
     expect(rebalanceTree.right.left.right).to.equal(null);
-    expect(rebalanceTree.right.right.value).to.equal(6);
+    expect(rebalanceTree.right.right.key).to.equal(6);
     expect(rebalanceTree.right.right.left).to.equal(null);
     expect(rebalanceTree.right.right.right).to.equal(null);
 
@@ -106,17 +105,17 @@ describe('binaryRebalanceTree', function() {
     rebalanceTree = rebalanceTree.insert(10);
 
     rebalanceTree = rebalanceTree.insert(1);
-    expect(rebalanceTree.value).to.equal(9);
-    expect(rebalanceTree.left.value).to.equal(8);
-    expect(rebalanceTree.left.left.value).to.equal(1);
+    expect(rebalanceTree.key).to.equal(9);
+    expect(rebalanceTree.left.key).to.equal(8);
+    expect(rebalanceTree.left.left.key).to.equal(1);
     expect(rebalanceTree.left.left.left).to.equal(null);
     expect(rebalanceTree.left.left.right).to.equal(null);
     expect(rebalanceTree.left.right).to.equal(null);
-    expect(rebalanceTree.right.value).to.equal(11);
-    expect(rebalanceTree.right.left.value).to.equal(10);
+    expect(rebalanceTree.right.key).to.equal(11);
+    expect(rebalanceTree.right.left.key).to.equal(10);
     expect(rebalanceTree.right.left.left).to.equal(null);
     expect(rebalanceTree.right.left.right).to.equal(null);
-    expect(rebalanceTree.right.right.value).to.equal(12);
+    expect(rebalanceTree.right.right.key).to.equal(12);
     expect(rebalanceTree.right.right.left).to.equal(null);
     expect(rebalanceTree.right.right.right).to.equal(null);
 
@@ -143,7 +142,7 @@ describe('binaryRebalanceTree', function() {
     rebalanceTree = rebalanceTree.insert(3);
 
     //expect(rebalanceTree.getMaxDepth()).to.equal(4);
-    expect(rebalanceTree.left.left.value).to.equal(11);
+    expect(rebalanceTree.left.left.key).to.equal(11);
     expect(rebalanceTree.left.left.left).to.equal(11);
 
   });
--- sprint-two/src/binaryRebalanceTree.js
+++ sprint-two/src/binaryRebalanceTree.js
@@ -1,259 +1,105 @@
+/*
+Used http://www.geeksforgeeks.org/avl-tree-set-1-insertion/ as a reference
+*/
 
-
-var BinaryRebalanceTree = function(value) {
+var BinaryRebalanceTree = function(key) {
   var binTree = Object.create(rebalanceTreeMethods);
   binTree.left = null;
   binTree.right = null;
-  //binTree.parent = null;
-  binTree.value = value;
-  //binTree.minDepth = 1;
-  //binTree.maxDepth = 1;
-  //binTree.depth = 1;
+  binTree.key = key;
+  binTree.height = 1;
   return binTree;
 };
 
 var rebalanceTreeMethods = {};
 
 
-
-rebalanceTreeMethods.insert = function(node, isNotRoot) {
-  this._insert(node);
-
-  /*
-  if ( (this.getMaxSide('right') - this.getMinSide('left')) === 2) {
-    return this.rebalanceLeft();
-  } else if ( (this.getMaxSide('left') - this.getMinSide('right')) === 2) {
-    return this.rebalanceRight();
-  }
-  return this;
-  */
-  //if(this.value === 2) debugger;
-
-  var sides = ['left', 'right'];
-  for(var i =0; i < 2; i++) {
-    for(var j =0; j < 2; j++) {
-      var nodeSide = sides[i];
-      var rebalanceSide = sides[j];
-      if(this[nodeSide]) {
-        if(this[nodeSide].needsRebalance(rebalanceSide)) {
-          console.log("rebalance " + nodeSide + " to " + rebalanceSide);
-          this[nodeSide] = this[nodeSide].rebalance(rebalanceSide);
-          break;
-        }
-      }
-    }
-  }
-
-  if(isNotRoot === false || isNotRoot === undefined)  {
-    if(this.needsRebalance('right')) {
-      return this.rebalanceRight();
-    } else if (this.needsRebalance('left')){
-      return this.rebalanceLeft();
-    }
-  }
-
-  return this;
+rebalanceTreeMethods.insert = function(key) {
+  var newRoot = insert(this, key);
+  return newRoot;
 };
-
-
-rebalanceTreeMethods._insert = function(node) {
-  //debugger;
-  var newTree = BinaryRebalanceTree(node);
-
-
-  if(node > this.value) {
-    if(this.right === null) {
-      this.right = newTree;
-      //newTree.depth = this.depth + 1;
-    } else {
-      this.right.insert(node, true);
-    }
-  } else {
-    if(this.left === null) {
-      this.left = newTree;
-      //newTree.depth = this.depth + 1;
-    } else {
-      this.left.insert(node, true);
-    }
-  }
-  // this.updateDepthRecursive();
-};
-
 
 rebalanceTreeMethods.getMaxDepth = function() {
-  var max = 1;
-  if (this.right !== null) {
-    max = Math.max(max, 1 + this.right.getMaxDepth());
-  }
-  if (this.left !== null) {
-    max = Math.max(max, 1 + this.left.getMaxDepth());
-  }
-
-  return max;
-
-};
-
-rebalanceTreeMethods.getMaxSide = function(side) {
-  var max = 1;
-  if (this[side] !== null ) {
-    max = Math.max(max, 1 + this[side].getMaxDepth());
-  }
-  return max;
+  return height(this);
 };
 
 
-rebalanceTreeMethods.getMinDepth = function() {
-  if(this.right === null) {
-    return 1;
+var height = function(N) {
+  if(N === null) {
+    return 0;
   }
-  if(this.left === null) {
-    return 1;
-  }
-  var min = 1 + this.right.getMinDepth();
-  min = Math.min(min, 1+this.left.getMinDepth());
-  return min;
-
+  return N.height;
 };
 
 
-rebalanceTreeMethods.getMinSide = function(side) {
-  if(this[side] == null) {
-    return 1;
-  }
-  return 1 + this[side].getMinDepth();
+var rightRotate = function(y) {
+  var x = y.left;
+  var T2 = x.right;
+
+  x.right = y;
+  y.left = T2;
+
+  y.height = Math.max(height(y.left), height(y.right)) + 1;
+  x.height = Math.max(height(x.left), height(x.right)) + 1;
+
+  return x;
 };
 
 
-rebalanceTreeMethods.needsRebalance = function(side) {
-  var otherSide;
-  if(side === "left"){
-    otherSide = "right";
+var leftRotate = function(x) {
+  var y = x.right;
+  var T2 = y.left;
+
+  y.left = x;
+  x.right = T2;
+
+  x.height = Math.max(height(x.left), height(x.right)) + 1;
+  y.height = Math.max(height(y.left), height(y.right)) + 1;
+
+  return y;
+};
+
+
+
+var getBalance = function(N) {
+  if (N == null) {
+    return 0;
+  }
+  return height(N.left) - height(N.right);
+};
+
+
+
+var insert = function(node, key) {
+  if(node === null) {
+    return  BinaryRebalanceTree(key);
+  }
+  if(key < node.key) {
+    node.left = insert(node.left, key);
   } else {
-    otherSide = "left";
+    node.right = insert(node.right, key);
+  }
+  node.height = Math.max(height(node.left), height(node.right)) + 1;
+
+  var balance = getBalance(node);
+
+  if ((balance > 1) && (key < node.left.key)) {
+    return rightRotate(node);
   }
 
-  if ( (this.getMaxSide(otherSide) - this.getMinSide(side)) === 2) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-
-rebalanceTreeMethods.rebalance = function(side) {
-  if(side === "left"){
-    return this.rebalanceLeft();
-  } else {
-    return this.rebalanceRight();
-  }
-};
-
-rebalanceTreeMethods.rebalanceRight = function() {
-  if(this.left && this.left.right) {
-    return this.reArrangeRight   ();
-  } else {
-    return this.rotateRight();
-  }
-};
-
-rebalanceTreeMethods.rebalanceLeft = function() {
-  if(this.right && this.right.left) {
-    return this.reArrangeLeft();
-  } else {
-    return this.rotateLeft();
-  }
-};
-
-
-rebalanceTreeMethods.rotateRight = function() {
-  var B = this;
-  var A = this.left;
-  B.left = A.right;
-  A.right = B;
-  return A;
-};
-
-rebalanceTreeMethods.rotateLeft = function() {
-
-  var A = this;
-  var B = this.right;
-  A.right = B.left;
-  B.left = A;
-  return B;
-  //this.updateDepthRecursive();
-};
-
-
-
-
-
-rebalanceTreeMethods.contains = function(node) {
-  if(node === this.value) {
-    return true;
-  } else if (node > this.value) {
-    if(this.right === null) {
-      return false;
-    } else {
-      return this.right.contains(node);
-    }
-  } else {
-    if(this.left === null) {
-      return false;
-    } else {
-      return this.left.contains(node);
-    }
+  if((balance < -1) && (key > node.right.key)) {
+    return leftRotate(node);
   }
 
-};
-
-rebalanceTreeMethods.depthFirstLog = function(cb) {
-  cb(this.value);
-  if(this.left != null) {
-    this.left.depthFirstLog(cb);
-  } 
-  if(this.right != null) {
-    this.right.depthFirstLog(cb);
+  if((balance > 1) && (key > node.left.key)) {
+    node.left = leftRotate(node.left);
+    return rightRotate(node);
   }
-};
 
-
-/*
-rebalanceTreeMethods.updateDepthRecursive = function() {
-  if((this.left === null) && (this.right === null)) {
-    this.maxDepth = 1;
-    this.minDepth = 1;
-  } else if (this.left === null) {
-    this.minDepth = 1;
-    this.maxDepth = 1 + this.right.maxDepth;
-  } else if (this.right === null) {
-    this.minDepth = 1;
-    this.maxDepth = 1 + this.left.maxDepth;
-  } else {
-    this.updateDepth();
+  if((balance < -1) && (key < node.right.key)) {
+    node.right = rightRotate(node.right);
+    return leftRotate(node);
   }
-};
 
-
-rebalanceTreeMethods.updateDepth = function() {
-  this.minDepth = 1 + Math.min(this.getMinSide('left'), this.getMinSide('right'));
-  this.maxDepth = 1 + Math.max(this.getMaxSide('left'), this.getMaxSide('right'));
+  return node;
 };
-
-rebalanceTreeMethods.getMinSide = function(prop) {
-  if(this[prop]) {return this[prop].minDepth;}
-  return 0;
-};
-
-rebalanceTreeMethods.getMaxSide = function(prop) {
-  if(this[prop]) {return this[prop].maxDepth;}
-  return 0;
-};
-
-rebalanceTreeMethods.getMaxDepth = function(node) {
-  return this.maxDepth;
-};
-
-rebalanceTreeMethods.getMinDepth = function(node) {
-  return this.minDepth;
-};
-*/
